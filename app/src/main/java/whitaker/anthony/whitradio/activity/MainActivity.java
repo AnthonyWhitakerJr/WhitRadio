f7717399a0e2725f3a9803a4c2d571d39f0dd7fa
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2017-04-20 22:57:36 -0400

    Load new full screen fragment on click.

--- app/src/main/java/whitaker/anthony/whitradio/activity/MainActivity.java
+++ app/src/main/java/whitaker/anthony/whitradio/activity/MainActivity.java
@@ -5,14 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import whitaker.anthony.whitradio.R;
+import whitaker.anthony.whitradio.fragment.DetailFragment;
 import whitaker.anthony.whitradio.fragment.MainFragment;
+import whitaker.anthony.whitradio.model.Station;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static MainActivity mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainActivity = this;
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         MainFragment mainFragment = (MainFragment)fragmentManager.findFragmentById(R.id.container_main);
@@ -21,5 +27,13 @@ public class MainActivity extends AppCompatActivity {
             mainFragment = MainFragment.newInstance(null, null);
             fragmentManager.beginTransaction().add(R.id.container_main, mainFragment).commit();
         }
+    }
+
+    public void loadDetailsScreen(Station selectedStation) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_main, new DetailFragment()).addToBackStack(null).commit();
+    }
+
+    public static MainActivity getMainActivity() {
+        return mainActivity;
     }
 }
--- app/src/main/java/whitaker/anthony/whitradio/adapter/StationAdapter.java
+++ app/src/main/java/whitaker/anthony/whitradio/adapter/StationAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import java.util.ArrayList;
 
 import whitaker.anthony.whitradio.R;
+import whitaker.anthony.whitradio.activity.MainActivity;
 import whitaker.anthony.whitradio.holder.StationViewHolder;
 import whitaker.anthony.whitradio.model.Station;
 
@@ -29,9 +30,16 @@ public class StationAdapter extends RecyclerView.Adapter<StationViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(StationViewHolder holder, int position) {
-        Station station = stations.get(position);
+    public void onBindViewHolder(StationViewHolder holder, final int position) {
+        final Station station = stations.get(position);
         holder.updateUI(station);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.getMainActivity().loadDetailsScreen(station);
+            }
+        });
     }
 
     @Override
