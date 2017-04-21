6c3f0a825c259d380cff17f162d876543d1182b1
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2017-04-20 21:40:40 -0400

    Designing cards and working with view holders

--- app/src/main/java/whitaker/anthony/whitradio/adapter/StationAdapter.java
+++ app/src/main/java/whitaker/anthony/whitradio/adapter/StationAdapter.java
@@ -1,10 +1,13 @@
 package whitaker.anthony.whitradio.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
+import whitaker.anthony.whitradio.R;
 import whitaker.anthony.whitradio.holder.StationViewHolder;
 import whitaker.anthony.whitradio.model.Station;
 
@@ -21,16 +24,18 @@ public class StationAdapter extends RecyclerView.Adapter<StationViewHolder> {
 
     @Override
     public StationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View stationCard = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_station, parent, false);
+        return new StationViewHolder(stationCard, parent.getContext());
     }
 
     @Override
     public void onBindViewHolder(StationViewHolder holder, int position) {
-
+        Station station = stations.get(position);
+        holder.updateUI(station);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return stations.size();
     }
 }
--- app/src/main/java/whitaker/anthony/whitradio/holder/StationViewHolder.java
+++ app/src/main/java/whitaker/anthony/whitradio/holder/StationViewHolder.java
@@ -1,13 +1,35 @@
 package whitaker.anthony.whitradio.holder;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import whitaker.anthony.whitradio.R;
+import whitaker.anthony.whitradio.model.Station;
 
 /**
  * @author Anthony Whitaker.
  */
 public class StationViewHolder extends RecyclerView.ViewHolder {
-    public StationViewHolder(View itemView) {
+
+    private ImageView stationImageView;
+    private TextView titleTextView;
+    private Context context;
+
+    public StationViewHolder(View itemView, @NonNull Context context) {
         super(itemView);
+
+        this.context = context;
+
+        this.stationImageView = (ImageView)itemView.findViewById(R.id.stationImage);
+        this.titleTextView = (TextView)itemView.findViewById(R.id.stationText);
+    }
+
+    public void updateUI(@NonNull Station station) {
+        stationImageView.setImageResource(station.getImageId());
+        titleTextView.setText(context.getString(station.getTitleId()));
     }
 }
